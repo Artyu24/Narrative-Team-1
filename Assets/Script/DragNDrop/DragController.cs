84873c775707dc8b6d1a67c9ca6ba6ef63c6d965
Antirion779 <73186199+Antirion779@users.noreply.github.com>
2023-01-20 11:53:05 +0100

    Squashed commit of the following:
    commit cf8f2d123326099557817125b77067ddc9da3392
    Author: Antirion779 <73186199+Antirion779@users.noreply.github.com>
    Date:   Fri Jan 20 11:52:48 2023 +0100
    
        [juiciness Pt.2]
    
    commit 6d55c8a3ee4e37ae1f6d2befbbc6001d1e47d69e
    Author: Antirion779 <73186199+Antirion779@users.noreply.github.com>
    Date:   Fri Jan 20 11:38:24 2023 +0100
    
        [Juiciness + TMP]
    
    commit 66e1104339dbbd4508bb3f7b51ee63eeab247e27
    Author: Antirion779 <73186199+Antirion779@users.noreply.github.com>
    Date:   Fri Jan 20 09:53:04 2023 +0100
    
        [First Drage + DotWeen Again]

---
+++ Assets/Script/DragNDrop/DragController.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Team1
+{    public class DragController : MonoBehaviour
+    {
+        [SerializeField] private bool isDragActive = false;
+        private Vector2 screenPosition;
+        private Vector3 worldPosition;
+        [SerializeField] private Draggable lastDragged;
+
+        private void Awake()
+        {
+            DragController[] controller = FindObjectsOfType<DragController>();
+            if (controller.Length > 1)
+                Destroy(gameObject);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isDragActive && (Input.GetMouseButtonUp(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended))) //Drop 
+            {
+                Drop();
+                return;
+            }
+
+            //input + Pos
+            if(Input.GetMouseButtonDown(0))
+            {
+                Vector3 _mousePos = Input.mousePosition;
+                screenPosition = new Vector2(_mousePos.x, _mousePos.y);
+            }
+            else if(Input.touchCount > 0)
+            {
+                screenPosition = Input.GetTouch(0).position;
+            }
+
+            worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+
+            if (isDragActive)
+                Drag();
+
+            else
+            {
+                RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
+                if(hit.collider != null && (Input.touchCount > 0) || Input.GetMouseButtonDown(0))
+                {
+                    if (hit.collider != null)
+                    {
+                        Draggable _draggable = hit.transform.GetComponent<Draggable>();
+                        if (_draggable != null)
+                        {
+                            lastDragged = _draggable;
+                            InitDrag();
+                        }
+                    }
+                    else
+                        Debug.Log("Nothing as been hited");
+                    
+                }
+            }
+        }
+
+        private void InitDrag()
+        {
+            isDragActive = true;
+        }
+
+        private void Drag()
+        {
+            lastDragged.transform.DOMove(new Vector2(worldPosition.x, worldPosition.y), .25f);
+            lastDragged.ValidateZone();
+        }
+
+        private void Drop() 
+        {
+            isDragActive = false;
+
+            if (lastDragged.hasToReturn)
+                lastDragged.transform.DOMove(lastDragged.InitPos, 1); lastDragged.transform.rotation = lastDragged.InitRot;
+
+            if (lastDragged.ValidateRight)
+                FindObjectOfType<TextMeshProUGUI>().text = "VALIDATE RIGHT SIDE";
+            else if (lastDragged.ValidateLeft)
+                FindObjectOfType<TextMeshProUGUI>().text = "VALIDATE LEFT SIDE";
+            else
+                FindObjectOfType<TextMeshProUGUI>().text = "<- WHAT DO YOU WANT TO DO ? ->";
+
+            lastDragged = null;
+        }
+    }
+}
