77dc90350cca40342ac1b3380cf7bf1926adc404
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2023-01-25 20:25:32 +0100

    v1 pnj et night transi

---
+++ Assets/nightandchara/Night.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+namespace Team01
+{
+    public class Night : MonoBehaviour
+    {
+        [SerializeField] private float duration;
+        [SerializeField] private SpriteRenderer spriteNight;
+        [SerializeField] private SpriteRenderer spriteLogo;
+
+        public void StartNightAnime()
+        {
+            Sequence NightAnime = DOTween.Sequence();
+
+            NightAnime.Insert(0, spriteNight.DOFade(0, 0));
+            NightAnime.Insert(0, spriteLogo.DOFade(0, 0f));
+            NightAnime.Append(spriteNight.DOFade(1, 2f));
+            NightAnime.Append(spriteLogo.DOFade(1, 1));
+            NightAnime.Append(spriteLogo.DOFade(0, 1));
+            NightAnime.Append(spriteNight.DOFade(0, 2));
+        }
+
+    }
+}
