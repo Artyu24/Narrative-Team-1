07f852172c4dfb9fb5e383976e9ad3047c02b2dd
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2023-01-29 11:13:37 +0100

    idleAnime

--- Assets/TeamOne/Script/GameFeel/CharacterAnimation.cs
+++ Assets/TeamOne/Script/GameFeel/CharacterAnimation.cs
@@ -8,18 +8,24 @@ namespace TeamOne
     public class CharacterAnimation : MonoBehaviour
     {
         [SerializeField] private SpriteRenderer charaRenderer;
-
-        private void Start()
-        {
-            ExitAnime();
-        }
+        Sequence idleAnime;
+        //private void Start()
+        //{
+        //    EnterAnime();
+        //}
         //elle tu l'appel au momen ou le perso spawnS
         public void InitCharacter(Sprite charasprite)
         {
             charaRenderer.sprite = charasprite;
             EnterAnime();
         }
-
+        private void Idle()
+        {
+            idleAnime = DOTween.Sequence();
+            idleAnime.Append(transform.DORotate(new Vector3(0, 0, -2),1f).SetEase(Ease.OutExpo));
+            idleAnime.Append(transform.DORotate(new Vector3(0, 0, 2), 1f).SetEase(Ease.OutExpo));
+            idleAnime.SetLoops(-1, LoopType.Yoyo);
+        }
         private void EnterAnime()
         {
             Sequence EnterAnime = DOTween.Sequence();
@@ -34,7 +40,7 @@ namespace TeamOne
             EnterAnime.Join(transform.DOMoveX(0f, 2f));
             EnterAnime.Join(transform.DORotate(new Vector3(0, 0, 0), 2f).SetEase(Ease.OutBack));
             EnterAnime.Insert(1.5f, charaRenderer.DOFade(1f, 2f));
-            EnterAnime.Join(charaRenderer.DOColor(Color.white, 2f));
+            EnterAnime.Join(charaRenderer.DOColor(Color.white, 2f)).OnComplete(()=>Idle());
         }
 
         //elle tu l'appel quand le dialogue est finis
