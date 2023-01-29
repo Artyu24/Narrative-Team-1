e4240cfe047ab4da2a788c3fe3305705b9551e2a
Artyu <arthurpejoiane@hotmail.fr>
2023-01-29 18:08:53 +0100

    Add New BackGround and New Settings menu

--- Assets/TeamOne/Script/Sound/AudioManager.cs
+++ Assets/TeamOne/Script/Sound/AudioManager.cs
@@ -6,98 +6,102 @@ using UnityEngine.UI;
 using UnityEngine.Audio;
 using Random = UnityEngine.Random;
 
-public class AudioManager : MonoBehaviour
+namespace TeamOne
 {
-    [SerializeField] private Sprite soundOn, soundOff;
-    [SerializeField] private Image buttonImage;
-    private bool soundActivate = true;
-
-    public Sound[] sounds;
-
-    public static AudioManager instance;
-    
-    private Dictionary<SoundState, List<Sound>> DicoActualSound = new Dictionary<SoundState, List<Sound>>();
-    
-    void Awake()
+    public class AudioManager : MonoBehaviour
     {
-        if(instance == null)
-            instance = this;
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
+        [SerializeField] private Sprite soundOn, soundOff;
+        [SerializeField] private Image buttonImage;
+        private bool soundActivate = true;
 
-        //Create an Audio Source for each sound
-        foreach (Sound s in sounds)
+        public Sound[] sounds;
+
+        public static AudioManager instance;
+        
+        private Dictionary<SoundState, List<Sound>> DicoActualSound = new Dictionary<SoundState, List<Sound>>();
+        
+        void Awake()
         {
-            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
-            s.source.loop = s.loop;
-            s.source.volume = s.volume;
-            s.source.outputAudioMixerGroup = s.audioMixer;
-        }
-        foreach (Sound sound in sounds)
-        {
-            if (!DicoActualSound.ContainsKey(sound.ActualSound))
+            if(instance == null)
+                instance = this;
+            else
             {
-                DicoActualSound.Add(sound.ActualSound, new List<Sound>());
+                Destroy(gameObject);
+                return;
             }
-            DicoActualSound[sound.ActualSound].Add(sound);
-        }
-    }
 
-    private void Start()
-    {
-        PlayRandom(SoundState.Victory);
-    }
-
-    public void Stop(SoundState soundState)
-    {
-        if (DicoActualSound.ContainsKey(soundState))
-        {
-            foreach (Sound s in DicoActualSound[soundState])
+            //Create an Audio Source for each sound
+            foreach (Sound s in sounds)
             {
+                s.source = gameObject.AddComponent<AudioSource>();
+                s.source.clip = s.clip;
+                s.source.loop = s.loop;
+                s.source.volume = s.volume;
+                s.source.outputAudioMixerGroup = s.audioMixer;
+            }
+            foreach (Sound sound in sounds)
+            {
+                if (!DicoActualSound.ContainsKey(sound.ActualSound))
+                {
+                    DicoActualSound.Add(sound.ActualSound, new List<Sound>());
+                }
+                DicoActualSound[sound.ActualSound].Add(sound);
+            }
+        }
+
+        private void Start()
+        {
+            PlayRandom(SoundState.Victory);
+        }
+
+        public void Stop(SoundState soundState)
+        {
+            if (DicoActualSound.ContainsKey(soundState))
+            {
+                foreach (Sound s in DicoActualSound[soundState])
+                {
+                    if (s == null)
+                    {
+                        Debug.LogWarning("Sound: " + name + " not found");
+                        return;
+                    }
+                    s.source.Stop();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Allows a random sound to be played if you have several different sounds for the same action
+        /// </summary>
+        public void PlayRandom(SoundState soundState)
+        {
+            if (DicoActualSound.ContainsKey(soundState) && soundActivate)
+            {
+                int i = Random.Range(0, DicoActualSound[soundState].Count);
+                Debug.Log("ça passe ");
+                Sound s = DicoActualSound[soundState][i];
                 if (s == null)
                 {
                     Debug.LogWarning("Sound: " + name + " not found");
                     return;
                 }
-                s.source.Stop();
+                s.source.Play();
             }
         }
-    }
 
-    /// <summary>
-    /// Allows a random sound to be played if you have several different sounds for the same action
-    /// </summary>
-    public void PlayRandom(SoundState soundState)
-    {
-        if (DicoActualSound.ContainsKey(soundState) && soundActivate)
+        /// <summary>
+        /// For the UI in Game
+        /// </summary>
+        public void SwitchSoundParameter()
         {
-            int i = Random.Range(0, DicoActualSound[soundState].Count);
-            Debug.Log("ça passe ");
-            Sound s = DicoActualSound[soundState][i];
-            if (s == null)
-            {
-                Debug.LogWarning("Sound: " + name + " not found");
-                return;
-            }
-            s.source.Play();
+            soundActivate = !soundActivate;
+
+            if (soundActivate)
+                buttonImage.sprite = soundOn;
+            
+            else
+                buttonImage.sprite = soundOff;
         }
-    }
-
-    /// <summary>
-    /// For the UI in Game
-    /// </summary>
-    public void SwitchSoundParameter()
-    {
-        soundActivate = !soundActivate;
-
-        if (soundActivate)
-            buttonImage.sprite = soundOn;
-        
-        else
-            buttonImage.sprite = soundOff;
     }
 }
+
